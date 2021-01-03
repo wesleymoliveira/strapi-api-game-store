d12cf99bd2dcefe146acc00ca635b58cf0a66bde
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-01-03 13:36:51 -0300

    create relations many to many data together

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -39,6 +39,35 @@ async function create(name, entityName) {
   }
 }
 
+async function createManyToManyData(products) {
+  const developers = {};
+  const publishers = {};
+  const categories = {};
+  const platforms = {};
+
+  products.forEach((product) => {
+    const { developer, publisher, genres, supportedOperatingSystems } = product;
+
+    genres &&
+      genres.forEach((item) => {
+        categories[item] = true;
+      });
+    supportedOperatingSystems &&
+      supportedOperatingSystems.forEach((item) => {
+        platforms[item] = true;
+      });
+    developers[developer] = true;
+    publishers[publisher] = true;
+  });
+
+  return Promise.all([
+    ...Object.keys(developers).map((name) => create(name, "developer")),
+    ...Object.keys(publishers).map((name) => create(name, "publisher")),
+    ...Object.keys(categories).map((name) => create(name, "category")),
+    ...Object.keys(platforms).map((name) => create(name, "platform")),
+  ]);
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -49,8 +78,10 @@ module.exports = {
 
     //console.log(products[0]);
 
-    await create(products[3].publisher, "publisher");
-    await create(products[3].developer, "developer");
+    await createManyToManyData([products[3], products[4]]);
+
+    //await create(products[3].publisher, "publisher");
+    //await create(products[3].developer, "developer");
 
     /* await strapi.services.publisher.create({
       name: products[0].publisher,
