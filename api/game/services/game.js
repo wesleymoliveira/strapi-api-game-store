1c3d8bafc21e7d1c6d89a0b3e15aedcff05ca7c9
Wesley M Oliveira <oliveirawesleyrj@gmail.com>
2021-01-03 12:31:59 -0300

    create game relations

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -23,6 +23,22 @@ async function getGameInfo(slug) {
   };
 }
 
+async function getByName(name, entityName) {
+  const item = await strapi.services[entityName].find({ name });
+  return item.length ? item[0] : null;
+}
+
+async function create(name, entityName) {
+  const item = await getByName(name, entityName);
+
+  if (!item) {
+    return await strapi.services[entityName].create({
+      name,
+      slug: slugify(name, { lower: true }),
+    });
+  }
+}
+
 module.exports = {
   populate: async (params) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -31,16 +47,19 @@ module.exports = {
       data: { products },
     } = await axios.get(gogApiUrl);
 
-    console.log(products[0]);
+    //console.log(products[0]);
 
-    await strapi.services.publisher.create({
+    await create(products[3].publisher, "publisher");
+    await create(products[3].developer, "developer");
+
+    /* await strapi.services.publisher.create({
       name: products[0].publisher,
       slug: slugify(products[0].publisher).toLowerCase(),
     });
     await strapi.services.developer.create({
       name: products[0].developer,
       slug: slugify(products[0].developer).toLowerCase(),
-    });
+    }); */
 
     //console.log(await getGameInfo(products[0].slug));
   },
